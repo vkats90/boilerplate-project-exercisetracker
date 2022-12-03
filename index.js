32bc8d63f1eaae3ce60ed3d4bd473ea1b6e42d4b
vkats90 <mars77@gmail.com>
2022-12-02 20:43:23 -0700

    commit

--- index.js
+++ index.js
@@ -3,6 +3,50 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+//mongoose config
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://mars77:1234@cluster0.xbzwd5f.mongodb.net/mongodb-fcc?retryWrites=true&w=majority', 
+{ 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+}); 
+
+const Exercise = new mongoose.Schema({
+  username: {
+    type:String,
+    required: true
+  },
+  description: String,
+  duration: Number,
+  date: Date,
+})
+
+const User = new mongoose.Schema({
+  username:{
+    type:String,
+    required:true
+  },
+  exercise:{
+    username: {
+      type:String,
+      required: true
+    },
+    description: {
+      type:String,
+      required: true
+    },
+    duration: {
+      type:Number,
+      required: true
+    },
+    date: {
+      type:Date,
+      required: true
+    },
+  }
+})
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
