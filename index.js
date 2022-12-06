fa501d37beef0b6c5d221172c36df920dd371e5d
vkats90 <mars77@gmail.com>
2022-12-05 17:31:40 -0700

    commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser');
 
 //mongoose config
 const mongoose = require('mongoose')
@@ -22,40 +23,102 @@ const Exercise = new mongoose.Schema({
   date: Date,
 })
 
-const User = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   username:{
     type:String,
     required:true
   },
-  exercise:{
-    username: {
-      type:String,
-      required: true
-    },
+  exercise:[{
     description: {
       type:String,
-      required: true
+      required: false
     },
     duration: {
       type:Number,
-      required: true
+      required: false
     },
     date: {
       type:Date,
-      required: true
+      required: false
     },
-  }
+  }]
 })
 
+const User = new mongoose.model('User',UserSchema);
+
+// MongoDB methods
+
+const errorHandle = (err) =>{
+  console.log("Trouble connecting to database"+err);
+}
+const findUserByName = async (uname) => {
+  try {
+    let found = await User.findOne({'username':uname});
+    return found
+  } catch {
+    errorHandle(err)
+  }
+}
+
+const listUsers = () => {
+  try {
+    const list = User.find().select('username')
+    return list;
+  } catch {
+    errorHandle(err);
+  }
+}
+
+const createUser = async (uname) => {
+  await User.create({'username':uname})
+}
+
+const findUserById = async (id) =>{
+  try {
+    let user = User.findById({_id:id});
+    return user;
+  } catch {
+    errorHandle(err)
+  }
+}
+const addExerciseToUser = async (id,desc,dur,date) => {
+  let user = await User.findUserById(id);
+  user.exercises.push({'description':desc,'duration':dur,'date':new date});
+  user.save();
+}
+
+// Middleware section
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// Http calls
 
+app.post('/api/users',async (req,res,next)=>{
+  let id = await findUserByName(req.body.username)
+  if (id == null) {
+    await createUser(req.body.username);
+    id = await findUserByName(req.body.username)
+  }
+  next()
+  },async (req,res)=>{
+    let id = await findUserByName(req.body.username)
+    res.json({"username":req.body.username,"id":id._id})
+  })
 
+app.get('/api/users',async (req,res)=>{
+  const list = await listUsers()
+  res.json(list); 
+})
 
+app.post('/api/users/:id/exercises',async (req,res)=>{
+  let user = await findUserById(req.params.id)
+  res.json({'_id':user._id,'username':user.username,'date':req.body.date,'duration':req.body.duration,'description':req.body.description})
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
